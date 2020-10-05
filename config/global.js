31d86be359ad1f3c419fb231f2c7b58c304818d7
Aldair <aldaprojects@gmail.com>
2020-10-05 15:17:27 -0500

    added global variables

---
+++ config/global.js
@@ -0,0 +1,17 @@
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+if ( process.env.NODE_ENV === 'dev' ) {
+    process.env.URL_DB = 'mongodb://localhost/covid';
+} else {
+    process.env.URL_DB = process.env.MONGO_URI || '';
+}
+
+process.env.PORT = process.env.PORT || 3000;
+
+const URL_DB = process.env.URL_DB;
+const PORT = process.env.PORT;
+
+module.exports = {
+    URL_DB,
+    PORT
+}
